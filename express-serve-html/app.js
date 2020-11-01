b05896fbf4ea163a83a0f89d69dc890567bbcd24
rgederin <gederin.ruslan@gmail.com>
2020-11-01 19:29:29 +0200

    move style in css

--- express-serve-html/app.js
+++ express-serve-html/app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const bodyParser = require('body-parser')
 
 const userRoutes = require('./routes/user');
+const { publicDecrypt } = require('crypto');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/user', userRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
