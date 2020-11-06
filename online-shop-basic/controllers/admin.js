8ee84ddb8b982840861cf8abf8ac78430aaa69dc
rgederin <gederin.ruslan@gmail.com>
2020-11-06 20:25:40 +0200

    extend routing in online shop

--- online-shop-basic/controllers/admin.js
+++ online-shop-basic/controllers/admin.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product');
+const { getProducts } = require('./shop');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -20,13 +21,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('admin/products', {
             prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
+            pageTitle: 'Admin products',
+            path: '/admin/products'
         });
     });
 };
