2556f609d19ed323d7cf70f300124432664a18ef
rgederin <gederin.ruslan@gmail.com>
2020-11-19 13:06:36 +0200

    first steps with mongo

--- online-shop-app-mongo/util/database.js
+++ online-shop-app-mongo/util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('onlineshop', 'root', 'root', {
-  dialect: 'mysql',
-  host: '127.0.0.1'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://rgederin:rownUovIp5jHzPQ2@cluster0.9acrc.mongodb.net/onlineshop?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('connected to mongodb in cloud')
+      _db = client.db('onlineshop');
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
