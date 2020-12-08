ceb104ab0fe8ae6bea3bbb00a567c7eb00cf5068
rgederin <gederin.ruslan@gmail.com>
2020-12-08 15:30:46 +0200

    add more validation

--- online-shop-app/controllers/admin.js
+++ online-shop-app/controllers/admin.js
@@ -1,14 +1,44 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator/check');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    errorMessage: '',
+    oldInput: {
+      title: '',
+      price: '',
+      confirmPassword: '',
+      description: '',
+      imageUrl: ''
+    },
+    validationErrors: []
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422)
+      .render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false,
+        errorMessage: errors.array()[0].msg,
+        oldInput: {
+          title: req.body.title,
+          price: req.body.price,
+          confirmPassword: req.body.confirmPassword,
+          description: req.body.description,
+          imageUrl: req.body.imageUrl
+        },
+        validationErrors: errors.array()
+      });
+  }
+
   const product = new Product({
     title: req.body.title,
     price: req.body.price,
--- online-shop-app/routes/admin.js
+++ online-shop-app/routes/admin.js
@@ -3,12 +3,28 @@ const express = require('express');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+const { check, body } = require('express-validator/check');
 
 const router = express.Router();
 
 router.get('/add-product', isAuth, adminController.getAddProduct);
+
 router.get('/products', isAuth, adminController.getProducts);
-router.post('/add-product', isAuth, adminController.postAddProduct);
+
+
+router.post('/add-product', isAuth, [
+    check('title', 'Title of the product should not be empty')
+        .notEmpty()
+        .trim(),
+    body('imageUrl', 'Image URL should not be empty and should be URL')
+        .notEmpty()
+        .isURL()
+        .trim(),
+    body('price', 'Product price should not be empty')
+        .notEmpty()
+        .trim()
+],
+    adminController.postAddProduct);
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 router.post('/edit-product', isAuth, adminController.postEditProduct);
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
